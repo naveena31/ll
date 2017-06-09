cee20c3762a1c239b244cd6853c3614e1cfd013e
naveena31 <naveena22896@gmail.com>
2017-06-09 15:24:38 +0530

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class vowel
+{
+	public static void main(String args[])
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextcharAt(0);
+		if(a=='a'||e=='e'||i=='i'||o=='o'||u=='u')
+		System.out.println("vowel");
+		else
+		System.out.println("consonant");
+	}
+		
+	}
